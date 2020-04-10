24a787a75a9c2d3bf469028ae1412ccefa4cfa85
coldice1915 <coldice.zs44@gmail.com>
2020-04-10 16:42:08 -0700

    complete

--- python/master-ticket/masterticket.py
+++ python/master-ticket/masterticket.py
@@ -1,40 +1,29 @@
+SERVICE_CHARGE = 2
 TICKET_PRICE = 10
 
 tickets_remaining = 100
 
+def calculate_price(total_tickets):
+    return (total_tickets * TICKET_PRICE) + SERVICE_CHARGE
 
-# Run this code continuously until we run out of tickets
 while tickets_remaining >= 1:
-    # TODO: Output how many tickets are remaining using the tickets_remaining
-    # variable.
     print("There are {} tickets remaining.".format(tickets_remaining))
-
-    # TODO: Gather user's name and assign it to new variable.
     name = input("What is your name?  ")
-
-    # TODO: prompt user by name and ask how many tickets they would like
-    total_tickets = int(input(
-        "Hello {}, how many tickets would you like?  ".format(name)))
-
-    # TODO: Calculate the price (# tickets * price) and assign it to variable
-    total_price = total_tickets*TICKET_PRICE
-
-    # TODO: Output price to screen
-    print("Your total is ${}.".format(total_price))
-
-    # TODO: prompt user if they want to proceed. Y/N?
-    proceed = input("Confirm purchase (Y/N). ")
-    proceed = proceed.upper()
-    # TODO: If they want to proceed:
-    #   print out to screen "SOLD!" to confirm purchase
-    #   and then decrement the tickets remaining by the number of tickets
-    #   purchased.
-    if proceed == "Y":
-        tickets_remaining -= total_tickets
-        print("SOLD! {} tickets remaining.".format(tickets_remaining))
+    try:
+        total_tickets = int(input(
+            "Hello {}, how many tickets would you like?  ".format(name)))
+        if total_tickets > tickets_remaining:
+            raise ValueError("Not enough tickets available. There are only {} tickets available".format(tickets_remaining))
+    except ValueError as err:
+        print("Error occurred! {}.".format(err))
     else:
-        # TODO: Else: thank them by name
-        print("Thank you {}.".format(name))
-
-# Notify user that the tickets are sold out
+        total_price = calculate_price(total_tickets)
+        print("Your total is ${}.".format(total_price))
+        proceed = input("Confirm purchase (Y/N). ")
+        proceed = proceed.upper()
+        if proceed == "Y":
+            tickets_remaining -= total_tickets
+            print("SOLD! {} tickets remaining.".format(tickets_remaining))
+        else:
+            print("Thank you {}.".format(name))
 print("Sorry, we're all sold out!")
